3bd0bf566b01e90a47a28a153b6dd9507308a899
Mo Salah <mohammedsalah71198@gmail.com>
2023-12-11 13:45:38 +0200

    time sheet

--- src/main/java/com/example/hrSystem/Dto/TimeSheetDetailDto.java
+++ src/main/java/com/example/hrSystem/Dto/TimeSheetDetailDto.java
@@ -4,20 +4,21 @@ import com.example.hrSystem.Dto.commen.GenericDto;
 import lombok.Data;
 
 
-
+import javax.persistence.Column;
 import java.time.LocalDate;
 @Data
 public class TimeSheetDetailDto extends GenericDto
 {
 
+
     private LocalDate date;
     private LocalDate overtimeDate;
-    private LocalDate taskId;
-    private LocalDate taskDesc;
+    private Float overtimeHours;
+    private Integer taskId;
+    private String taskDesc;
     private Boolean approvalStatus;
     private Boolean billable;
     private String comments;
-    private Float overtimeHours;
     private Float startTime;
     private Float endTime;
     private Float timeHours;
